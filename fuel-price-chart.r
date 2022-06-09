4cd5ceae417311325cc254a56181b9b15f1b6503
Rob Hanssen <rob.hanssen@outlook.com>
2022-06-09 10:24:02 -0400

    better prediction method

--- fuel-price-chart.r
+++ fuel-price-chart.r
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(lubridate)
 load("Rdata/fuel.Rdata")
 
 cutoff_date <- as.Date("2020-03-29")
@@ -11,6 +12,17 @@ predict_fuel_price <-
     broom::augment(
         interval = "prediction",
         newdata = tibble(date = seq(cutoff_date - 39, today() + 40, 1))
+    ) %>%
+    rename(price = ".fitted")
+
+
+predict_fuel_price_postinvasion <-
+    fuel %>%
+    filter(date > invasion) %>%
+    lm(price ~ date, data = .) %>%
+    broom::augment(
+        interval = "prediction",
+        newdata = tibble(date = seq(invasion, today() + 40, 1))
     ) %>%
     rename(price = ".fitted")
 
@@ -47,13 +59,19 @@ fuel %>%
         alpha = .1,
         fill = "blue"
     ) +
-    geom_vline(xintercept = as.Date("2021-01-21"), lty = 3) +
-    geom_vline(xintercept = as.Date("2022-02-24"), lty = 3) +
-    annotate("label",
-        x = as.Date("2021-01-21"),
-        y = .20,
-        label = "Inauguration\nBiden"
+    geom_line(
+        data = predict_fuel_price_postinvasion,
+        lty = 2,
+        color = "gray50",
+        alpha = .7
     ) +
+    geom_ribbon(
+        data = predict_fuel_price_postinvasion,
+        aes(ymin = .lower, ymax = .upper),
+        alpha = .1,
+        fill = "orange"
+    ) +
+    geom_vline(xintercept = invasion, lty = 3) +
     annotate("label",
         x = as.Date("2022-02-24"),
         y = .20,
@@ -62,3 +80,72 @@ fuel %>%
     theme_light()
 
 ggsave("graphs/fuel-price-postCOVID.png", width = 6, height = 6)
+
+
+models <-
+    fuel %>%
+    mutate(event = cut(date,
+        breaks = c(cutoff_date - years(100), cutoff_date, invasion, today()),
+        label = c("ignore", "post-COVID", "post-invasion")
+    )) %>%
+    filter(event != "ignore") %>%
+    group_by(event) %>%
+    nest() %>%
+    mutate(pricemodel = map(data, ~ lm(price ~ date, data = .x)))
+
+predict_multi <-
+    models %>%
+    mutate(pricepred = map(pricemodel, 
+            ~ broom::augment(.x, 
+                            interval = "prediction",
+                            newdata = tibble(date = seq(cutoff_date, 
+                                            today() + 61, 
+                                            1)
+                                            )
+                                )
+                    )) %>%
+    unnest(pricepred) %>%
+    rename(price = ".fitted")
+
+fuel %>%
+    filter(date >= cutoff_date) %>%
+    ggplot() +
+    aes(x = date, y = price) +
+    geom_point(alpha = .3) +
+    labs(
+        x = "Date",
+        y = "Fuel price (in $/gal)",
+        caption = "Fuel prices in South Carolina.\nBlue band indicater confidence interval of prediction",
+        color = "Timeframe",
+        fill = "Timeframe"
+    ) +
+    scale_x_date(
+        date_labels = "%b\n %Y",
+        date_breaks = "3 months",
+        limits = c(cutoff_date - 40, today() + 61)
+    ) +
+    scale_y_continuous(
+        labels = scales::dollar_format(accuracy = .01),
+        limit = c(0, NA),
+        sec.axis = sec_axis(~., labels = scales::dollar_format(accuracy = .01))
+    ) +
+    geom_line(
+        data = predict_multi,
+        lty = 2,
+        aes(color = event),
+        alpha = .7
+    ) +
+    geom_ribbon(
+        data = predict_multi,
+        aes(ymin = .lower, ymax = .upper, fill = event),
+        alpha = .1
+    ) +
+    geom_vline(xintercept = invasion, lty = 3) +
+    annotate("label",
+        x = as.Date("2022-02-24"),
+        y = .20,
+        label = "Invasion of\nUkraine"
+    ) +
+    theme_light()
+
+ggsave("graphs/fuel-price-by-event.png", width = 8, height = 6)
