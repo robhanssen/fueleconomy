d4516a03397a7baf9050c95994dec5115b680e7a
Rob Hanssen <rob.hanssen@outlook.com>
2023-05-18 14:46:10 -0400

    linting

--- dev/mpg-analysis-allcars.r
+++ dev/mpg-analysis-allcars.r
@@ -35,9 +35,9 @@ fuel_cdf <-
     mutate(car_name = factor(car_name)) %>%
     split(.$car_name) %>%
     map(\(df)
-        map_dbl(
-            mpg_range,
-            \(m) nrow(filter(df, mpg < m)) / nrow(df)
+    map_dbl(
+        mpg_range,
+        \(m) nrow(filter(df, mpg < m)) / nrow(df)
     )) %>%
     enframe() %>%
     unnest(value) %>%
@@ -55,24 +55,32 @@ last_fuelups <-
 
 cap <- glue::glue("Note: dots indicate last {last_points} fuel-up mpg data")
 
-mpg_cdf_p <- 
+mpg_cdf_p <-
     fuel_cdf %>%
     ggplot(aes(x = mpg, y = mpg_cdf, color = car_name)) +
     geom_line(show.legend = FALSE, alpha = .3) +
     geom_jitter(data = last_fuelups, aes(y = 0.5, x = mpg), height = .1) +
-    scale_y_continuous(labels = scales::percent_format(), 
-        breaks = seq(0,1,length.out = 5)) +
+    scale_y_continuous(
+        labels = scales::percent_format(),
+        breaks = seq(0, 1, length.out = 5)
+    ) +
     labs(
         x = "Fuel economy", y = "Cumulative density function",
         caption = cap
     ) +
-    geom_segment(data = alltime_avg,
+    geom_segment(
+        data = alltime_avg,
         alpha = .3, linewidth = 3,
-        aes(x = all_mpg, xend = all_mpg,
-            y = 0, yend = 1)) +
-    facet_wrap(~car_name, ncol = 1, scale = "free_x") + 
-    theme(legend.position = "none") + 
+        aes(
+            x = all_mpg, xend = all_mpg,
+            y = 0, yend = 1
+        )
+    ) +
+    facet_wrap(~car_name, ncol = 1, scale = "free_x") +
+    theme(legend.position = "none") +
     scale_color_manual(values = c("gray10", "purple", "red"))
 
-ggsave("graphs/fuelmpg_cdf.png", height = 8, width = 6, 
-        plot = mpg_cdf_p)
+ggsave("graphs/fuelmpg_cdf.png",
+    height = 8, width = 6,
+    plot = mpg_cdf_p
+)
